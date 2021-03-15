5333468f2965599ae8b0646767eb4c81093f4314
henrymon342 <henrymon342@gmail.com>
2021-03-15 10:48:13 -0400

    version 1.0

---
+++ app/src/main/java/com/example/uhf_bt/view/Articulo.java
@@ -0,0 +1,18 @@
+package com.example.uhf_bt.view;
+
+public class Articulo {
+
+    private String codigo;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Articulo(String n){
+        codigo = n;
+    }
+}
