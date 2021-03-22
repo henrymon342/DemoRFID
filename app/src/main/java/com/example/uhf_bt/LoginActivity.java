a7b67cb9c3cfc7dc1732b9adb11cd1673a240c12
evolutionv5 <jorgeluiscastrov5@gmail.com>
2021-03-22 19:33:21 -0400

    login creado

--- app/src/main/java/com/example/uhf_bt/LoginActivity.java
+++ app/src/main/java/com/example/uhf_bt/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.uhf_bt;
 
 import android.annotation.TargetApi;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,25 +12,35 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import com.example.uhf_bt.Utilidades.utilidades;
 import no.nordicsemi.android.dfu.BuildConfig;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class LoginActivity extends BaseActivity {
-    private Button btn_login;
+    private Button btn_login,btn_registro;
     private TextView correo,password;
     @TargetApi(Build.VERSION_CODES.GINGERBREAD_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        btn_login=(Button)findViewById(R.id.btn_login);
 
+        ConectionSQLiteHelper conn=new ConectionSQLiteHelper(this,"bdUser",null,1);
+
+        btn_login=(Button)findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //setContentView(MainActivity);
                 logear();
+            }
+        });
+
+        btn_registro=(Button)findViewById(R.id.btn_registro);
+        btn_registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrar();
             }
         });
     }
@@ -44,7 +56,21 @@ public class LoginActivity extends BaseActivity {
         }else{
             Toast.makeText(this, "usuario o constraseña invalido", Toast.LENGTH_SHORT).show();
         }
+    }
+    public void registrar(){
 
+        ConectionSQLiteHelper conn=new ConectionSQLiteHelper(this,"bdUser",null,1);
+        SQLiteDatabase db=conn.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(utilidades.CAMPO_ID,"u1");
+        values.put(utilidades.CAMPO_NOMBRE,"jorge");
+        values.put(utilidades.CAMPO_EMAIL,"jorge@gmail.com");
+        values.put(utilidades.CAMPO_PASSWORD,"1234567");
+
+        Long idResultante=db.insert(utilidades.TABLA_USUARIO,utilidades.CAMPO_ID,values);
+
+        Toast.makeText(getApplicationContext(),"Id Registro: "+idResultante,Toast.LENGTH_SHORT).show();
+        db.close();
     }
 
 
