8c061e2e1eeba64f5ed53b20407f67dfded1ed70
jorgeCastro <jorgeluiscastrov5@gmail.com>
2021-07-11 20:14:40 -0400

    busqueda Activity modified

--- app/src/main/java/com/example/uhf_bt/BusquedaActivity.java
+++ app/src/main/java/com/example/uhf_bt/BusquedaActivity.java
@@ -42,6 +42,7 @@ public class BusquedaActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 pedirUbicacion();
+                getSearchList();
                 //getRfidSQLite();
             }
         });
